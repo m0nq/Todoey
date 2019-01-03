ddd83e28cdd4fa22dcce24b5b0c8698c3bcb57ce
Monk <monk@industriallogic.com>
2019-01-02 17:14:15 -0800

    Added tableview number of rows and cell for row at a specified index

--- Todoey/Todoey/ToDoListViewController.swift
+++ Todoey/Todoey/ToDoListViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
+    let items = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
     }
 }
 
