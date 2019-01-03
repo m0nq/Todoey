c714389f305671651d657175c8b95bfa5c7dcd93
Monk <monk@industriallogic.com>
2019-01-02 17:05:23 -0800

    Setup initial project with uitable view

---
+++ Todoey/Todoey/ToDoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by Monk Wellington on 1/2/19.
+//  Copyright © 2019 Industrial Logic. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
